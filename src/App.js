2c20b5e3d21f3a5671c65844d317513483f67f3f
Arsha <arshaskrd97@gmail.com>
2021-07-18 14:57:18 +0530

    Add redux

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { Provider } from 'react-redux';
+import React from 'react';
+// import CakeContainer from './components/CakeContainer'
+// import HooksCakeContainer from './components/HooksCakeContainer'
+// import IceCreamContainer from './components/IceCreamContainer'
+// import NewCakeContainer from './components/NewCakeContainer'
+// import ItemContainer from './components/ItemContainer'
+import UsersContainer from './components/UsersContainer';
+import store from './redux/store';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <UsersContainer />
+        {/* <ItemContainer cake />
+        <ItemContainer />
+        <NewCakeContainer />
+        <CakeContainer />
+        <HooksCakeContainer />
+        <IceCreamContainer /> */}
+      </div>
+    </Provider>
   );
 }
 
---
+++ src/redux/store.js
@@ -0,0 +1,13 @@
+import { applyMiddleware, createStore } from 'redux';
+
+import { composeWithDevTools } from 'redux-devtools-extension';
+import logger from 'redux-logger';
+import rootReducer from './rootReducer';
+import thunk from 'redux-thunk';
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger, thunk))
+);
+
+export default store;
